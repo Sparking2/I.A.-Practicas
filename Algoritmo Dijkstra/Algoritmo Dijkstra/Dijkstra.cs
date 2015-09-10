e1aa8dfaa0b1db1f9108bbe240455aa6a69a9c5f
Sparking2 <sparking02@yahoo.com.mx>
2015-09-10 11:48:02 -0500

    Dijkstra C# v2

--- Algoritmo Dijkstra/Algoritmo Dijkstra/Dijkstra.cs
+++ Algoritmo Dijkstra/Algoritmo Dijkstra/Dijkstra.cs
@@ -8,7 +8,33 @@ namespace Algoritmo_Dijkstra
     { //variables a usar
         private int rango = 0;
         private int[,] L; //Matriz de adyacencia
+        private int[] C; //Arreglo de Nodos
+        private int[] D; //Arreglo de Distancias
+        private int trango = 0;
+        
+        //Algoritmo Dijkstra
+        public Dijkstra(int paramRango, int[,]paramArreglo)
+        {
+            L = new int[paramRango, paramRango];
+            C = new int[paramRango];
+            D = new int[paramRango];
+            rango = paramRango;
 
+            for (int i=0;i<rango;i++)
+            {
+                for (int j = 0;j<rango;j++)
+                {
+                    L[i, j] = paramArreglo[i, j];
+                }
+            }
+            for (int i = 0;i<rango;i++)
+            {
+                C[i] = i;
+            }
+            C[0] = -1;
+            for (int i = 1; i < rango; i++)
+                D[i] = L[0, i];
+        }
         static void Main(string[] args)
         {
         }
