f089bf74a3cf51ba3ec8c7f608120bed53f514ad
Sparking2 <sparking02@yahoo.com.mx>
2015-09-10 11:30:51 -0500

    Dijkstra C# v0

--- Dijkstra/main.cpp
+++ Dijkstra/main.cpp
@@ -1,19 +1,2 @@
-#define MAX 10005
-#define Node pair< int , int >
-#define INF 1<<30
 
-#include <iostream>
-vector< Node > ady[ MAX ];
-int distancia[ MAX ];
-bool visitado[ MAX ];
-int previo[ MAX ];
-priority_queue< Node , vector<Node> , cmp > Q;
-int V;
 
-using namespace std;
-
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
-}
