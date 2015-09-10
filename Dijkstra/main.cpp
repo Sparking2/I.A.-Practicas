c879b704714563e394c1070efdf10e8cba6a620a
Sparking2 <sparking02@yahoo.com.mx>
2015-09-10 10:33:58 -0500

    v0.1

--- Dijkstra/main.cpp
+++ Dijkstra/main.cpp
@@ -1,4 +1,14 @@
+#define MAX 10005
+#define Node pair< int , int >
+#define INF 1<<30
+
 #include <iostream>
+vector< Node > ady[ MAX ];
+int distancia[ MAX ];
+bool visitado[ MAX ];
+int previo[ MAX ];
+priority_queue< Node , vector<Node> , cmp > Q;
+int V;
 
 using namespace std;
 
